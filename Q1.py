a14a3ce083ab4e24394598ec500740b0592d949b
sahnawaz <sahnawaz@moltis.net>
2022-08-01 17:35:45 +0530

    Add Extra code in Q1

--- Q1.py
+++ Q1.py
@@ -12,3 +12,32 @@ def reverseElement(a, s, e):
 a = [23, 4, 2, 76, 22, 90, 21]
 reverseElement(a, 0, 6)
 print("Reverse Element : ", a)
+
+
+EXTRA : 
+#for string
+
+def reverseElement(a, s, e):
+    while s < e:
+        a[s], a[e] = a[e], a[s]
+        s += 1
+        e -= 1
+
+
+a = ["shaan", "rohit", "maisa", "maitry", "somil", "akki", "sam"]
+reverseElement(a, 0, 6)
+print("Reverse Element : ", a)
+
+****************************************************************
+
+#reverse a proper string
+
+def nameReverse(l1):
+    for i in l1:
+        res = l1[::-1]
+    print(res)
+
+
+l1 = "shaan"
+nameReverse(l1)
+print(l1)
