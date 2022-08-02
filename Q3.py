bdeaec8c158c73ab6e4ef6f348682874391515f6
SahnawazShaban <shaanshaban400@gmail.com>
2022-08-02 20:29:22 +0530

    #4

--- Q3.py
+++ Q3.py
@@ -10,3 +10,14 @@ mini = [2, 5, 3, 88, 23, 45, 12, 10, 0, -1]
 n = len(mini)
 kth = int(input("Enter Kth value : "))
 minMax(mini, n, kth)
+
+
+Difference between sort() vs sorted().
+EXAMPLE : 
+nums = [2, 3, 1, 5, 6, 4, 0]
+
+print(sorted(nums))   # [0, 1, 2, 3, 4, 5, 6]
+print(nums)           # [2, 3, 1, 5, 6, 4, 0]
+
+print(nums.sort())    # None
+print(nums)           # [0, 1, 2, 3, 4, 5, 6]
---
+++ Q4.py
@@ -0,0 +1,16 @@
+# Q3 : Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo.
+
+abc = [1, 1, 2, 2, 2, 2, 1, 1, 2, 0, 0, 1, 2, 1]
+
+for i in range(len(abc)):
+    if abc[i] == 0:
+        print(abc[i], end=" ")
+
+for i in range(len(abc)):
+    if abc[i] == 1:
+        print(abc[i], end=" ")
+
+for i in range(len(abc)):
+    if abc[i] == 2:
+        print(abc[i], end=" ")
+
