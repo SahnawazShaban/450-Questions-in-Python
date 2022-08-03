7a0eaff6169dea594efea636ad186b8b966ed1a9
SahnawazShaban <shaanshaban400@gmail.com>
2022-08-03 11:14:00 +0530

    #4

--- Q4.py
+++ Q4.py
@@ -1,4 +1,4 @@
-# Q3 : Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo.
+# Q4 : Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo.
 
 abc = [1, 1, 2, 2, 2, 2, 1, 1, 2, 0, 0, 1, 2, 1]
 
