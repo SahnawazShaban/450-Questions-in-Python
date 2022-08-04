5d9a3ba8288f64b72ef690e07fe2506a576cdfad
SahnawazShaban <shaanshaban400@gmail.com>
2022-08-04 17:00:55 +0530

    #7

--- Q7.py
+++ Q7.py
@@ -7,3 +7,21 @@ for i in range(0, a):
     j = i - 1
     arr[j] = arr[i]
     print(arr[i])
+    
+    
+********************************************
+
+
+Using Function : 
+
+def cyclically(arr, a):
+    for i in range(0, a):
+        j = i - 1
+        arr[j] = arr[i]
+        print(arr[i])
+
+
+arr = [1, 4, 6, 2, 4, 5, 7, 9]
+a = len(arr) - 1
+print(arr[a])
+cyclically(arr, a)
